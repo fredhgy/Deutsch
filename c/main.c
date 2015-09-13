e2e866b342a9bdc38995fa6eb47f62364169ba12
fredhgy <fredhgy2010@gmail.com>
2015-09-13 17:29:44 +0800

    定冠词部分完成
    包含基本的软件框架

--- c/main.c
+++ c/main.c
@@ -5,13 +5,19 @@
 void menu()
 {
 	system("cls");
-	printf("hello\n");
+	printf("欢迎使用\n");
+	printf("德语语法查询软件\n");
+	printf("资料收集，软件制作:何广宇\n");
+	printf("网站:www.heguangyu.net\n");
+	printf("邮箱:me@heguangyu.net\n");
+	 
 }
 
 void main()
 {
 	int n;
 	menu();
+	printf("请输入代码:"); 
 	scanf("%d",&n);
 	while(n)
 	{
@@ -23,16 +29,24 @@ void main()
 			case 2:
 				xingrongci();
 				break;
+			default:
+				break;
 		}
 		getch();
 		menu();
-		scanf("input:%d",&n);
+		printf("继续使用请输入序号，退出请输0:\n");
+		scanf("%d",&n);
 	}
+	printf("谢谢使用，欢迎指正改进\n");
+	printf("项目托管地址:github.com/fredhgy\n");
+	system("pause");
 }
 
 void guancidaici()
 {
 	int a;
+	//system("cls");
+	printf("请输入代码1:"); 
 	scanf("%d",&a);
 	switch(a)
 		{
@@ -52,7 +66,12 @@ void xingrongci()
 
 void dingguanci()
 {
-	printf("111");
+	printf("|     格    | 阳性 | 中性 | 阴性 | 复数 |\n");
+	printf("| --------- | ---- | ---- | ---- | ---- |\n");
+	printf("| 第一格 N. | der  | das  | die  | die  |\n");
+	printf("| 第二格 G. | des  | des  | der  | der  |\n");
+	printf("| 第三格 D. | dem  | dem  | der  | den  |\n");
+	printf("| 第四格 A. | den  | das  | die  | die  |\n");
 }
 
 void budingguanci()
